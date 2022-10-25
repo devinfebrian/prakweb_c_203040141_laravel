d06a85628ef5dd5262e32405edd05792a6bbfa58
Devin Febrian <devinfebrian20@gmail.com>
2022-10-26 00:02:54 +0700

    Stuktur folder, Routes, dan View

--- prakweb_c_203040141/routes/web.php
+++ prakweb_c_203040141/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Devin Febrian",
+        "email" => "devinfebrian20@gmail.com",
+        "image" => "devin.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
