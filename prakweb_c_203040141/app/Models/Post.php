3be4936a44b69c6fe655131fa8293bc42e91595a
Devin Febrian <devinfebrian20@gmail.com>
2022-10-26 21:35:57 +0700

    Model, Collection & Controllers

---
+++ prakweb_c_203040141/app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+
+
+class Post
+{
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Devin Febrian",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deleniti obcaecati esse dolores quia facilis quo, dolorem consectetur voluptas temporibus, culpa doloremque illo praesentium omnis ipsum vel molestias porro maxime adipisci quasi! Cum velit, similique esse fugit ipsam animi assumenda recusandae earum cupiditate at odio molestiae veritatis dicta nemo error, eos labore facere asperiores pariatur quaerat obcaecati placeat blanditiis doloremque. Excepturi vitae consectetur ducimus quisquam quo incidunt corporis voluptates voluptatibus delectus, suscipit, perferendis quod architecto ex? Qui iste vitae fugiat!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul=post-kedua",
+            "author" => "Chairil Daffa",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea labore exercitationem asperiores nisi debitis officiis doloribus nam necessitatibus, enim tempore pariatur dicta nemo quibusdam error quis aliquid optio voluptates quos quae accusamus magnam fugit. Eum accusamus voluptatibus facere et maiores dolorum adipisci assumenda repudiandae. Quia ab inventore, dolores asperiores numquam impedit? Veniam fugit voluptatum, praesentium facilis laudantium iusto voluptatem reiciendis nam corporis hic. Dolor, impedit adipisci nostrum ad illo pariatur quaerat laboriosam? Commodi laudantium, natus quia amet odio facere sint dolore nam. Hic, consequuntur adipisci earum eum aperiam quos reiciendis consectetur in dicta culpa pariatur obcaecati ipsam quibusdam ducimus cumque."
+        ]
+    ];
+
+    public static function all()
+    {
+        return collect(self::$blog_posts);
+    }
+
+    public static function find($slug)
+    {
+        $posts = static::all();
+        return $posts->firstWhere('slug', $slug);
+    }
+}
--- prakweb_c_203040141/routes/web.php
+++ prakweb_c_203040141/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,54 +31,5 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Devin Febrian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deleniti obcaecati esse dolores quia facilis quo, dolorem consectetur voluptas temporibus, culpa doloremque illo praesentium omnis ipsum vel molestias porro maxime adipisci quasi! Cum velit, similique esse fugit ipsam animi assumenda recusandae earum cupiditate at odio molestiae veritatis dicta nemo error, eos labore facere asperiores pariatur quaerat obcaecati placeat blanditiis doloremque. Excepturi vitae consectetur ducimus quisquam quo incidunt corporis voluptates voluptatibus delectus, suscipit, perferendis quod architecto ex? Qui iste vitae fugiat!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul=post-kedua",
-            "author" => "Chairil Daffa",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea labore exercitationem asperiores nisi debitis officiis doloribus nam necessitatibus, enim tempore pariatur dicta nemo quibusdam error quis aliquid optio voluptates quos quae accusamus magnam fugit. Eum accusamus voluptatibus facere et maiores dolorum adipisci assumenda repudiandae. Quia ab inventore, dolores asperiores numquam impedit? Veniam fugit voluptatum, praesentium facilis laudantium iusto voluptatem reiciendis nam corporis hic. Dolor, impedit adipisci nostrum ad illo pariatur quaerat laboriosam? Commodi laudantium, natus quia amet odio facere sint dolore nam. Hic, consequuntur adipisci earum eum aperiam quos reiciendis consectetur in dicta culpa pariatur obcaecati ipsam quibusdam ducimus cumque."
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Devin Febrian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deleniti obcaecati esse dolores quia facilis quo, dolorem consectetur voluptas temporibus, culpa doloremque illo praesentium omnis ipsum vel molestias porro maxime adipisci quasi! Cum velit, similique esse fugit ipsam animi assumenda recusandae earum cupiditate at odio molestiae veritatis dicta nemo error, eos labore facere asperiores pariatur quaerat obcaecati placeat blanditiis doloremque. Excepturi vitae consectetur ducimus quisquam quo incidunt corporis voluptates voluptatibus delectus, suscipit, perferendis quod architecto ex? Qui iste vitae fugiat!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul=post-kedua",
-            "author" => "Chairil Daffa",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea labore exercitationem asperiores nisi debitis officiis doloribus nam necessitatibus, enim tempore pariatur dicta nemo quibusdam error quis aliquid optio voluptates quos quae accusamus magnam fugit. Eum accusamus voluptatibus facere et maiores dolorum adipisci assumenda repudiandae. Quia ab inventore, dolores asperiores numquam impedit? Veniam fugit voluptatum, praesentium facilis laudantium iusto voluptatem reiciendis nam corporis hic. Dolor, impedit adipisci nostrum ad illo pariatur quaerat laboriosam? Commodi laudantium, natus quia amet odio facere sint dolore nam. Hic, consequuntur adipisci earum eum aperiam quos reiciendis consectetur in dicta culpa pariatur obcaecati ipsam quibusdam ducimus cumque."
-        ]
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts/{slug}', [PostController::class, 'show']);
