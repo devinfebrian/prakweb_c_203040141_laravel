5340311eb089dfcdb70fb1b2142dec2d0a447fac
Devin Febrian <devinfebrian20@gmail.com>
2022-10-31 03:33:30 +0700

    Database Seeder

--- prakweb_c_203040141/database/seeders/DatabaseSeeder.php
+++ prakweb_c_203040141/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,63 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Devin Febrian',
+            'email' => 'devinfebrian20@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Athala Thahira',
+            'email' => 'athalathahira@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempore culpa sunt omnis provident nesciunt autem error aspernatur deserunt labore eius, architecto tempora, repellendus quam nam facilis assumenda consequatur odio.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non exercitationem voluptatum nulla quia laudantium! Iusto asperiores quo blanditiis natus quod molestiae, quos ea temporibus enim! Ea, dicta voluptate eaque minima magnam architecto iusto in neque! Nobis quia minima eveniet iure, repudiandae fugiat totam iste, veritatis quo quam sunt fugit cumque, reprehenderit necessitatibus repellat? Placeat mollitia repellendus ipsum excepturi. Suscipit, dolore.</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod debitis, dolorum quidem temporibus omnis eius? Laboriosam dolores illum sed odio recusandae ea nemo et enim blanditiis magnam iusto ad consequatur, deleniti vel laborum incidunt, laudantium earum inventore! Enim nihil esse itaque sed nostrum vero. Sequi dolores sint ex culpa aut distinctio temporibus, inventore, porro saepe, perferendis labore similique. Dolor, temporibus?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempore culpa sunt omnis provident nesciunt autem error aspernatur deserunt labore eius, architecto tempora, repellendus quam nam facilis assumenda consequatur odio.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non exercitationem voluptatum nulla quia laudantium! Iusto asperiores quo blanditiis natus quod molestiae, quos ea temporibus enim! Ea, dicta voluptate eaque minima magnam architecto iusto in neque! Nobis quia minima eveniet iure, repudiandae fugiat totam iste, veritatis quo quam sunt fugit cumque, reprehenderit necessitatibus repellat? Placeat mollitia repellendus ipsum excepturi. Suscipit, dolore.</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod debitis, dolorum quidem temporibus omnis eius? Laboriosam dolores illum sed odio recusandae ea nemo et enim blanditiis magnam iusto ad consequatur, deleniti vel laborum incidunt, laudantium earum inventore! Enim nihil esse itaque sed nostrum vero. Sequi dolores sint ex culpa aut distinctio temporibus, inventore, porro saepe, perferendis labore similique. Dolor, temporibus?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempore culpa sunt omnis provident nesciunt autem error aspernatur deserunt labore eius, architecto tempora, repellendus quam nam facilis assumenda consequatur odio.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non exercitationem voluptatum nulla quia laudantium! Iusto asperiores quo blanditiis natus quod molestiae, quos ea temporibus enim! Ea, dicta voluptate eaque minima magnam architecto iusto in neque! Nobis quia minima eveniet iure, repudiandae fugiat totam iste, veritatis quo quam sunt fugit cumque, reprehenderit necessitatibus repellat? Placeat mollitia repellendus ipsum excepturi. Suscipit, dolore.</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod debitis, dolorum quidem temporibus omnis eius? Laboriosam dolores illum sed odio recusandae ea nemo et enim blanditiis magnam iusto ad consequatur, deleniti vel laborum incidunt, laudantium earum inventore! Enim nihil esse itaque sed nostrum vero. Sequi dolores sint ex culpa aut distinctio temporibus, inventore, porro saepe, perferendis labore similique. Dolor, temporibus?',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempore culpa sunt omnis provident nesciunt autem error aspernatur deserunt labore eius, architecto tempora, repellendus quam nam facilis assumenda consequatur odio.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non exercitationem voluptatum nulla quia laudantium! Iusto asperiores quo blanditiis natus quod molestiae, quos ea temporibus enim! Ea, dicta voluptate eaque minima magnam architecto iusto in neque! Nobis quia minima eveniet iure, repudiandae fugiat totam iste, veritatis quo quam sunt fugit cumque, reprehenderit necessitatibus repellat? Placeat mollitia repellendus ipsum excepturi. Suscipit, dolore.</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod debitis, dolorum quidem temporibus omnis eius? Laboriosam dolores illum sed odio recusandae ea nemo et enim blanditiis magnam iusto ad consequatur, deleniti vel laborum incidunt, laudantium earum inventore! Enim nihil esse itaque sed nostrum vero. Sequi dolores sint ex culpa aut distinctio temporibus, inventore, porro saepe, perferendis labore similique. Dolor, temporibus?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
